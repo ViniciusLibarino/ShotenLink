f3fd351bb328f3a0f0783843ef61db48099bd54e
Vinicius Libarino <viny.liba013@gmail.com>
2021-06-02 19:11:30 -0300

    componente modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Modal
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StatusBarPage from "../../components/StatusBarPage";
 import Menu from "../../components/Menu";
@@ -14,43 +21,74 @@ import {
   BoxIcon,
   Input,
   ButtonLink,
-  ButtonLinkText
+  ButtonLinkText,
 } from "./styles";
+import ModalLink from "../../components/ModalLink";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleShortLink(){
+    //alert('URL digitada: ' + input)
+
+    setModalVisible(true)
+  }
+
   return (
-    <LinearGradient
-      colors={["#1ddbb9", "#132742"]}
-      style={{ flex: 1, justifyContent: "center" }}
-    >
-      <StatusBarPage barStyle="light-content" backgroundColor="#1ddbb9" />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient
+        colors={["#1ddbb9", "#132742"]}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <StatusBarPage barStyle="light-content" backgroundColor="#1ddbb9" />
 
-      <Menu />
+        <Menu />
 
-      <ContainerLogo>
-        <Logo source={require("../../assets/Logo.png")} resizeMode="contain" />
-      </ContainerLogo>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "android" ? "padding" : "position"}
+          enabled
+        >
+          <ContainerLogo>
+            <Logo
+              source={require("../../assets/Logo.png")}
+              resizeMode="contain"
+            />
+          </ContainerLogo>
 
-      <ContainerContent>
-        <Title>ShortenLink</Title>
-        <Subtitle>Cole seu link para encurtar</Subtitle>
+          <ContainerContent>
+            <Title>ShortenLink</Title>
+            <Subtitle>Cole seu link para encurtar</Subtitle>
 
-        <ContainerInput>
-          <BoxIcon>
-            <Feather name="link" size={22} color="#fff" />
-          </BoxIcon>
+            <ContainerInput>
+              <BoxIcon>
+                <Feather name="link" size={22} color="#fff" />
+              </BoxIcon>
 
-          <Input 
-            placeholder="Cole seu link aqui..." 
-            placeholderTextColor="white"
-          />
-        </ContainerInput>
+              <Input
+                placeholder="Cole seu link aqui..."
+                placeholderTextColor="white"
+                autoCapitalize="none"
+                autoCorrect={false}
+                KeyboardType="url"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+              />
+            </ContainerInput>
 
-        <ButtonLink>
-          <ButtonLinkText>Gerar Link</ButtonLinkText>
-        </ButtonLink>
-      </ContainerContent>
+            <ButtonLink onPress={handleShortLink}>
+              <ButtonLinkText>Gerar Link</ButtonLinkText>
+            </ButtonLink>
 
-    </LinearGradient>
+          </ContainerContent>
+
+        </KeyboardAvoidingView>
+
+        <Modal visible={modalVisible} transparent animationType='slide'>
+          <ModalLink onClose={() => setModalVisible(false)}/>
+        </Modal>
+
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
